6ebeb2d01259a8b826c4edcf9a39ba5453481ba9
Ivan Passos <ivanpassosfilho@gmail.com>
2023-10-05 08:43:42 -0300

    Aula 25 - Animando abertura dos Selects

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,5 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
+module.exports = {
+  content: ['./app/**/*.tsx'],
   theme: {
     extend: {
       gridTemplateColumns: {
@@ -9,8 +7,35 @@ const config: Config = {
         profile: 'max-content 1fr max-content',
         form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
+      borderWidth: {
+        6: '6px',
+      },
+      colors: {
+        violet: {
+          25: '#fcfaff',
+        },
+      },
+      maxWidth: {
+        app: '700px',
+      },
+
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: 0, transform: 'translateY(-2px)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
+        // slideUpAndFade: {
+        //   from: { opacity: 1 },
+        //   to: { opacity: 0 },
+        // },
+      },
+
+      animation: {
+        slideDownAndFade:
+          'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+        // slideUpAndFade: 'slideUpAndFade 1s linear',
+      },
     },
   },
   plugins: [],
 }
-export default config
